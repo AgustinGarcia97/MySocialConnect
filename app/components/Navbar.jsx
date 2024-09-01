f77270ccd1b062085142cb5ef8a09b16df29b92b
Agustin <aggarcia997@gmail.com>
2024-09-01 17:18:03 -0300

    Proceso del navbar (en produccion)

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { Appbar } from 'react-native-paper';
+export const Navbar = () => {
+    return(
+        <Appbar.Header>
+            <Appbar.BackAction onPress={() => {}} />
+            <Appbar.Content title="Title" />
+            <Appbar.Action icon="calendar" onPress={() => {}} />
+            <Appbar.Action icon="magnify" onPress={() => {}} />
+        </Appbar.Header>
+        )
+
+}
