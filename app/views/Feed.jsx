bc17d1f34785156e54c7874c09a533bb31d10384
Agustin <aggarcia997@gmail.com>
2024-09-11 20:28:11 -0300

    eliminacion de imports

--- app/views/Feed.jsx
+++ app/views/Feed.jsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { Avatar, Button, Card, Text } from 'react-native-paper';
 import {ScrollView, View} from "react-native";
-import {styles} from "../assets/styles/home_style";
+
 import {Post} from "../components/feed_components/Post";
 import {InputPost} from "../components/feed_components/post_components/InputPost";
 import {feedStyles} from "../assets/styles/feed/feed_style";
