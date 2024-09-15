0722c5ac7cee118180faf7e97c6345c2d351e5f1
Agustin <aggarcia997@gmail.com>
2024-09-15 03:59:58 -0300

    implementacion del registro (paso 1)

--- app/assets/styles/register/register_style.js
+++ app/assets/styles/register/register_style.js
@@ -5,17 +5,23 @@ export const register_style = {
         height:'100%',
     },
     register_logo_container:{
-        height:100,
-
+        height:140,
         width:'100%',
+        justifyContent:'center',
+        alignItems:'center',
+
+
+    },
+    logo:{
+        marginTop:40,
+        width: 450,
+        height: 250,
     },
     register_text_container:{
         height: 280,
-
         width:'100%',
     },
     register_title_container:{
-
         height:'65%',
         width:'100%',
         justifyContent:'center',
@@ -26,7 +32,14 @@ export const register_style = {
         width:'60%',
         textAlign:'center',
         fontWeight:'600',
-
+        textShadowColor: '#3f3e3e',
+        textShadowOffset: { width: -1, height: 1 },
+        textShadowRadius: 1,
+    },
+    shadow_subtitle:{
+        textShadowColor: '#3f3e3e',
+        textShadowOffset: { width: -1, height: 1 },
+        textShadowRadius: 1,
     },
     register_subtitle_container:{
         height:'20%',
@@ -81,5 +94,87 @@ export const register_style = {
         color:"#3aa2f3",
         fontSize:16,
     }
+
+}
+
+export const createAccountStyle = {
+    create_account_container:{
+        height:'auto',
+
+    },
+    logo_title_container:{
+        justifyContent:'center',
+        alignItems:'center',
+        padding:10,
+
+    },
+    logo_title:{
+        fontSize:20,
+        fontWeight:'bold',
+        marginTop: 0,
+        marginBottom:10,
+    },
+    subtitle:{
+        marginTop:10,
+        color:'#636667',
+        fontWeight:'200',
+    },
+    inputs_texts_container:{
+        width:'100%',
+        justifyContent:'center',
+        alignItems:'center',
+        padding:2,
+    },
+    input_text_container:{
+        width:'90%',
+        padding:2,
+        margin:5,
+
+    },
+    text_input:{
+        backgroundColor:'#e7e6e6',
+
+
+
+    },
+    terms_conditions_container:{
+        width:'90%',
+
+        flexDirection:'row',
+        flexWrap:'wrap',
+        justifyContent:'center',
+        alignItems:'center',
+        marginBottom:20,
+
+    },
+    terms_conditions:{
+        fontWeight:'100',
+        color:'#636667'
+    },
+    next_button_container:{
+
+        marginVertical: 150,
+        justifyContent:'center',
+        alignItems:'center',
+        width:'100%',
+
+    },
+    next_button:{
+        height:50,
+        width:380,
+        backgroundColor:'#f36815',
+        justifyContent:'center',
+        alignItems:'center',
+        margin:10,
+        borderRadius: 10,
+    },
+    text_button:{
+        color:'#fff',
+        fontWeight:'bold',
+        fontSize:17,
+    },
+
+
+
 
 }
--- app/views/Register.jsx
+++ app/views/Register.jsx
@@ -1,20 +1,24 @@
-import {TouchableOpacity, View} from "react-native";
+import {TouchableOpacity, View, Image} from "react-native";
 import {Button, Text} from "react-native-paper";
 import  Icon from 'react-native-vector-icons/FontAwesome'
 import {register_style} from "../assets/styles/register/register_style";
 import {GoogleIcon} from "../assets/icons/GoogleIcon";
+
 const buttons = [
     {
         name: 'google',
-        text: 'Registrate con Google'
+        text: 'Registrate con Google',
+        navigate: 'RegisterGoogle'
     },
     {
         name: 'apple',
-        text: 'Registrate con Apple'
+        text: 'Registrate con Apple',
+        navigate: "RegisterApple"
     },
     {
         name: '',
-        text: 'Registrate con el mail o numero de movil'
+        text: 'Registrate con el mail o numero de movil',
+        navigate: 'CreateAccount'
     }
 ]
 
@@ -23,19 +27,24 @@ export const Register = ({navigation}) => {
     return(
         <View style={{...register_style.register_container}}>
             <View style={{...register_style.register_logo_container}}>
-                <Text style={{}}>Logo app title</Text>
+                <Image source={require('../assets/logos/logo.png')}
+                    style={{...register_style.logo}} resizeMode="cover"
+                ></Image>
+
             </View>
             <View style={{...register_style.register_text_container}}>
                 <View style={{...register_style.register_title_container}}>
-                    <Text style={{...register_style.register_title}}>My Social Connect</Text>
+                    <Text style={{...register_style.register_title}}>
+                        <Text style={{...register_style.shadow_subtitle,color:'#f5d03d'}}>My</Text> <Text style={{...register_style.shadow_subtitle,color:'#d007a4'}}>Social</Text> <Text style={{...register_style.shadow_subtitle,  color:'#0cdea6'}}>Connect</Text>
+                    </Text>
                 </View>
                 <View style={{...register_style.register_subtitle_container}}>
-                    <Text style={{...register_style.register_subtitle}}>Unite a MySocialConnect hoy mismo.</Text>
+                    <Text style={{...register_style.register_subtitle}}>Unite a <Text style={{...register_style.shadow_subtitle,color:'#f5d03d'}}>My</Text> <Text style={{...register_style.shadow_subtitle,color:'#d007a4'}}>Social</Text> <Text style={{...register_style.shadow_subtitle,  color:'#0cdea6'}}>Connect</Text> hoy mismo.</Text>
                 </View>
             </View>
             <View style={{...register_style.register_options_container}}>
                 {buttons.map((button, index) => (
-                        <TouchableOpacity onPress={() => {}} index={index}>
+                        <TouchableOpacity onPress={() => navigation.navigate(button.navigate)} key={index}>
                             <View style={{...register_style.register_option_button,
                                 backgroundColor:button.name==='apple'?'#000':'#fff'}}>
                                 {button.name==='google'?
@@ -46,11 +55,8 @@ export const Register = ({navigation}) => {
                                         size={25}
                                         style={{ marginRight: 8, display:button.name===''?"none":'flex' }}
                                         color={button.name==='apple'?'#fff':''}>
-
                                     </Icon>
-
                                 }
-
                                 <Text style={{...register_style.text_button,color:button.name==='apple'?'#fff':''}}>{button.text}</Text>
                             </View>
                         </TouchableOpacity>
