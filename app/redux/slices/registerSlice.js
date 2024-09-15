ca5be210e613a33bb3298c590459a91b6b79c1d7
Agustin <aggarcia997@gmail.com>
2024-09-15 03:58:50 -0300

    Creacion de un slice para el componente de register

---
+++ app/redux/slices/registerSlice.js
@@ -0,0 +1,52 @@
+import {createSlice} from "@reduxjs/toolkit";
+import {modalSlice} from "./modalSlice";
+
+const initialState = {
+    name:"",
+    email:"",
+    phone_number:"",
+    password:"",
+    birthdate:"",
+}
+
+export const registerSlice = createSlice({
+    name:'register',
+    initialState,
+    reducers:{
+        setNameSlice: (state, action) => {
+            state.name = action.payload.name;
+            alert(state.name);
+
+
+        },
+        setEmailSlice: (state, action) => {
+            state.email = action.payload.email;
+        },
+        setPhoneNumberSlice: (state, action) => {
+            state.phone_number = action.payload.phone_number;
+        },
+        setPasswordSlice: (state, action) => {
+            state.password = action.payload.password;
+        },
+        setBirthdateSlice: (state, action) => {
+            state.birthdate = action.payload.birthdate;
+        },
+        deleteDataSlice:(state, action) => {
+            state.name = "";
+            state.email = "";
+            state.phone_number = "";
+            state.password = "";
+        }
+    }
+
+
+});
+export const {
+    setNameSlice,
+    setEmailSlice,
+    setPhoneNumberSlice,
+    setPasswordSlice,
+    setBirthdateSlice,
+    deleteDataSlice,
+} = registerSlice.actions;
+
--- app/redux/store.js
+++ app/redux/store.js
@@ -1,9 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import {modalSlice} from "./slices/modalSlice";
+import {registerSlice} from "./slices/registerSlice";
+
 
 const store = configureStore({
     reducer:{
         modal: modalSlice.reducer,
+        register: registerSlice.reducer,
     },
 });
 
