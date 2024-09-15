98afe8bbfc506437403f7e5769a8915f3d9fe2cb
Agustin <aggarcia997@gmail.com>
2024-09-15 03:59:19 -0300

    Implementacion de otras rutas con opciones de navegacion

--- app/navigation/Navigation.jsx
+++ app/navigation/Navigation.jsx
@@ -5,6 +5,12 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import {CustomHeaderButton} from "./navigation_components/CustomHeaderButton";
 import {Register} from "../views/Register";
 import {Login} from "../views/Login";
+import {CreateAccount} from "../components/register_components/CreateAccount";
+import {CreateByApple} from "../components/register_components/CreateByApple";
+import {CreateByGoogle} from "../components/register_components/CreateByGoogle";
+import {CreatePassword} from "../components/register_components/registration_steps/CreatePassword";
+import {Button, TouchableOpacity} from "react-native";
+import Icon from "react-native-vector-icons/Ionicons";
 
 
 const Drawer = createDrawerNavigator();
@@ -21,11 +27,82 @@ export const Navigation = () => {
                     headerRight: () => <CustomHeaderButton navigation={navigation} />,
                     drawerPosition: 'right',
                     headerLeft: () => null,
-                })}>
-                <Drawer.Screen name="Feed" component={Feed}  />
-                <Drawer.Screen name="Profile" component={Profile} />
-                <Drawer.Screen name="Register" component={Register} options={{ headerShown: false }}/>
-                <Drawer.Screen name="Login" component={Login}/>
+
+                })}
+
+
+            >
+                <Drawer.Screen name="Feed" component={Feed}   options={{ headerTitle:'' }}  />
+                <Drawer.Screen name="Profile" component={Profile}  options={ ({navigation}) => (
+                    { headerTitle:'',headerLeft: () => (
+                            <TouchableOpacity onPress={() => navigation.navigate('Feed')}>
+                                <Icon
+                                    name={'chevron-back'}
+                                    size={30}
+                                /></TouchableOpacity>
+                        ) })}/>
+                <Drawer.Screen name="Register" component={Register}
+                               options={ ({navigation}) => (
+                                   { headerTitle:'',headerLeft: () => (
+                                           <TouchableOpacity onPress={() => navigation.navigate('Feed')}>
+                                               <Icon
+                                                   name={'chevron-back'}
+                                                   size={30}
+                                               /></TouchableOpacity>
+                                       ) })}/>
+                <Drawer.Screen name="Login" component={Login}
+                               options={ ({navigation}) => (
+                                    { headerTitle:'',headerLeft: () => (
+                                <TouchableOpacity onPress={() => navigation.navigate('Feed')}>
+                                    <Icon
+                                        name={'chevron-back'}
+                                        size={30}
+                                    /></TouchableOpacity>
+                        ) })}/>
+                <Drawer.Screen name="CreateAccount" component={CreateAccount}
+                               options={ ({navigation}) => (
+                                    { headerTitle:'',drawerItemStyle:{display:'none'},headerLeft: () => (
+                                        <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+                                            <Icon
+                                                name={'chevron-back'}
+                                                size={30}
+                                            /></TouchableOpacity>
+                        ) })}/>
+
+                <Drawer.Screen name="RegisterApple" component={CreateByApple}
+                               options={ ({navigation}) => (
+                                   { headerTitle:'',drawerItemStyle:{display:'none'},headerLeft: () => (
+                                           <TouchableOpacity onPress={() => navigation.navigate('CreateAccount')}>
+                                               <Icon
+                                                   name={'chevron-back'}
+                                                   size={30}
+                                               />
+                                           </TouchableOpacity>
+
+                                       ) })}/>
+
+                <Drawer.Screen name="RegisterGoogle" component={CreateByGoogle}
+                               options={ ({navigation}) => (
+                                   { headerTitle:'',drawerItemStyle:{display:'none'},headerLeft: () => (
+                                           <TouchableOpacity onPress={() => navigation.navigate('CreateAccount')}>
+                                               <Icon
+                                                   name={'chevron-back'}
+                                                   size={30}
+                                               />
+                                           </TouchableOpacity>
+
+                                       ) })}/>
+                <Drawer.Screen name="CreatePassword" component={CreatePassword}
+                               options={ ({navigation}) => (
+                    { headerTitle:'',drawerItemStyle:{display:'none'},headerLeft: () => (
+                            <TouchableOpacity onPress={() => navigation.navigate('CreateAccount')}>
+                                <Icon
+                                    name={'chevron-back'}
+                                    size={30}
+                                />
+                            </TouchableOpacity>
+
+                    ) })}/>
             </Drawer.Navigator>
         </NavigationContainer>
     )
