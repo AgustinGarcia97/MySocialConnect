78066c0b458bc56cf7e7bf492426d867f5d6a74c
Agustin <aggarcia997@gmail.com>
2024-11-14 17:20:44 -0300

    Ultimos updates

--- app/api/token/fetch_auth.js
+++ app/api/token/fetch_auth.js
@@ -8,7 +8,7 @@ export const fetch_register = async (data,dispatch) => {
             },
             body: JSON.stringify(data)
         }
-        const response = await fetch("http://10.0.2.2:8080/api/v1/auth/register", options);
+        const response = await fetch("http://socialconnectserver-env-2.eba-39bs2mf3.us-east-1.elasticbeanstalk.com/api/v1/auth/register", options);
         return  response.ok() === true;
     } catch(error){
         console.log("Error al registrarse:",error)
